4600363f95f4fac457de556dfcc888fb10d4dd33
wyattw99 <wwolfersberger@gmail.com>
2023-12-14 20:42:31 -0600

    Start of Java App
    If time permitting this will be finished

---
+++ ExerciseTrackerInterface/src/Exercise.java
@@ -0,0 +1,88 @@
+import java.util.Date;
+
+public class Exercise {
+	
+	private int exercise_id;
+	private String exercise_name;
+	private String exercise_type;
+	private int exercise_distance;
+	private String exercise_description;
+	private int user_id;
+	private Date exercise_date;
+	private int exercise_time;
+	
+	public int getExercise_id() {
+		return exercise_id;
+	}
+	public void setExercise_id(int exercise_id) {
+		this.exercise_id = exercise_id;
+	}
+	public String getExercise_name() {
+		return exercise_name;
+	}
+	public void setExercise_name(String exercise_name) {
+		this.exercise_name = exercise_name;
+	}
+	public String getExercise_type() {
+		return exercise_type;
+	}
+	public void setExercise_type(String exercise_type) {
+		this.exercise_type = exercise_type;
+	}
+	public int getExercise_distance() {
+		return exercise_distance;
+	}
+	public void setExercise_distance(int exercise_distance) {
+		this.exercise_distance = exercise_distance;
+	}
+	public String getExercise_description() {
+		return exercise_description;
+	}
+	public void setExercise_description(String exercise_description) {
+		this.exercise_description = exercise_description;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public Date getExercise_date() {
+		return exercise_date;
+	}
+	public void setExercise_date(Date exercise_date) {
+		this.exercise_date = exercise_date;
+	}
+	public int getExercise_time() {
+		return exercise_time;
+	}
+	public void setExercise_time(int exercise_time) {
+		this.exercise_time = exercise_time;
+	}
+	
+	public Exercise(int exercise_id, String exercise_name, String exercise_type, int exercise_distance,
+			String exercise_description, int user_id, Date exercise_date, int exercise_time) {
+		this.exercise_id = exercise_id;
+		this.exercise_name = exercise_name;
+		this.exercise_type = exercise_type;
+		this.exercise_distance = exercise_distance;
+		this.exercise_description = exercise_description;
+		this.user_id = user_id;
+		this.exercise_date = exercise_date;
+		this.exercise_time = exercise_time;
+	}
+	
+	public Exercise() {
+		this.exercise_name = "";
+		this.exercise_type = "";
+		this.exercise_distance = 0;
+		this.exercise_description = "";
+		this.user_id = 0;
+		this.exercise_date = null;
+		this.exercise_time = 0;
+	}
+	
+	
+	
+	
+}
---
+++ ExerciseTrackerInterface/src/User.java
@@ -0,0 +1,74 @@
+import java.util.Date;
+
+public class User {
+	
+	private int user_id;
+	private String user_name;
+	private String user_password;
+	private String first_name;
+	private String last_name;
+	private Date user_birthday;
+	private int user_height;
+	private int user_weight;
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_password() {
+		return user_password;
+	}
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public Date getUser_birthday() {
+		return user_birthday;
+	}
+	public void setUser_birthday(Date user_birthday) {
+		this.user_birthday = user_birthday;
+	}
+	public int getUser_height() {
+		return user_height;
+	}
+	public void setUser_height(int user_height) {
+		this.user_height = user_height;
+	}
+	public int getUser_weight() {
+		return user_weight;
+	}
+	public void setUser_weight(int user_weight) {
+		this.user_weight = user_weight;
+	}
+	public User(int user_id, String user_name, String user_password, String first_name, String last_name,
+			Date user_birthday, int user_height, int user_weight) {
+		this.user_id = user_id;
+		this.user_name = user_name;
+		this.user_password = user_password;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.user_birthday = user_birthday;
+		this.user_height = user_height;
+		this.user_weight = user_weight;
+	}
+	
+	
+}
